b35b1b0679f1eea02eb712033bb6c519b78ada69
Abdulrhman <Abdoulrhmanit@gmail.com>
2020-05-15 20:03:03 +0300

    Dockrize Gateway service

--- Gateways/MobileApiGateway/Program.cs
+++ Gateways/MobileApiGateway/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace MobileApiGateway
 {
@@ -14,7 +17,37 @@ namespace MobileApiGateway
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                // uncomment to write to Azure diagnostics stream
+                //.WriteTo.File(
+                //    @"D:\home\LogFiles\Application\identityserver.txt",
+                //    fileSizeLimitBytes: 1_000_000,
+                //    rollOnFileSizeLimit: true,
+                //    shared: true,
+                //    flushToDiskInterval: TimeSpan.FromSeconds(1))
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting host...");
+                CreateHostBuilder(args).Build().Run();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly.");
+                return;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- Gateways/MobileApiGateway/Startup.cs
+++ Gateways/MobileApiGateway/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.Middleware;
 using Ocelot.DependencyInjection;
+using Serilog;
 
 namespace MobileApiGateway
 {
@@ -26,14 +27,19 @@ namespace MobileApiGateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var url = Configuration["SwaggerEndPoints:Configs:Url"];
+            Log.Information("The url used in the swagger end point is: " + url);
             services.AddControllers();
-            services.AddOcelot();
+            services.AddOcelot(Configuration);
             services.AddSwaggerForOcelot(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var url = Configuration["SwaggerEndPoints:Configs:Url"];
+            Log.Information("The url used in the swagger end point is: " + url);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
