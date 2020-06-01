4390d488f4aab61c766a5fc0156d37bd4c32b7b5
Abdulrhman <Abdoulrhmanit@gmail.com>
2020-06-01 21:43:06 +0300

    Add User Profile Picture to User

---
+++ Services/Catalog/Catalog.DataAccessLayer/Migrations/20200601184211_AddPicUrlToUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Catalog.DataAccessLayer.Migrations
+{
+    public partial class AddPicUrlToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePicture",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "Users");
+        }
+    }
+}
