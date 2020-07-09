d2e952b2e76908010bfad75c4d44f7be44f020fa
Abdulrhman <Abdoulrhmanit@gmail.com>
2020-07-09 17:50:40 +0300

    Transaction: Fixes to models

--- Services/Transaction/Transaction.Service/Program.cs
+++ Services/Transaction/Transaction.Service/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
+using Unity.Microsoft.DependencyInjection;
 
 namespace Transaction.Service
 {
@@ -51,6 +52,15 @@ namespace Transaction.Service
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseUnityServiceProvider()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", true, true)
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                        .AddEnvironmentVariables();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
