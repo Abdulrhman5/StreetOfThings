8acd9ce2b4ddfe63b863721d429418d837e99b19
Abdulrhman <Abdoulrhmanit@gmail.com>
2020-07-30 19:45:01 +0300

    Auth: Patch: Add Roles,UserRoles

--- Services/Authorization/AuthorizationService/ConfigDatabases.cs
+++ Services/Authorization/AuthorizationService/ConfigDatabases.cs
@@ -6,8 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AuthorizationService
 {
@@ -16,23 +14,41 @@ namespace AuthorizationService
 
         public static void SeedUsersDatabase(IApplicationBuilder app)
         {
-            using(var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
             {
                 var databaseContext = serviceScope.ServiceProvider.GetRequiredService<AuthorizationContext>();
                 databaseContext.Database.Migrate();
 
                 var manager = serviceScope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
-                foreach(var user in Users)
+                foreach (var user in Users)
                 {
                     if (manager.FindByNameAsync(user.UserName).GetAwaiter().GetResult() is null)
                     {
                         manager.CreateAsync(user, "1qa2ws#ED").GetAwaiter().GetResult();
                     }
                 }
+
+                var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                foreach (var role in Roles)
+                {
+                    if(roleManager.FindByNameAsync(role.Name).GetAwaiter().GetResult() is null)
+                    {
+                        roleManager.CreateAsync(role).GetAwaiter().GetResult();
+                    }
+                }
+
+                foreach(var userRole in UserRoles)
+                {
+                    var user = manager.FindByEmailAsync(userRole.Item1.Email).GetAwaiter().GetResult();
+                    if(!manager.IsInRoleAsync(user, userRole.Role.Name).GetAwaiter().GetResult())
+                    {
+                        manager.AddToRoleAsync(user, userRole.Role.Name).GetAwaiter().GetResult();
+                    }
+                }
             }
         }
 
-        private static IEnumerable<AppUser> Users =>
+        private static List<AppUser> Users =>
             new List<AppUser>
             {
                 new AppUser
@@ -46,6 +62,23 @@ namespace AuthorizationService
                     CreatedAt = DateTime.UtcNow,
                     Gender = Gender.Male,
                 }
+            };
+
+        private static List<IdentityRole> Roles =>
+            new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = "1",
+                    Name = "Admin",
+                    NormalizedName = "Admin"
+                }
+            };
+
+        private static IEnumerable<(AppUser User, IdentityRole Role)> UserRoles =>
+            new List<(AppUser User, IdentityRole Role)>
+            {
+                (Users[0],Roles[0])
             };
     }
 }
