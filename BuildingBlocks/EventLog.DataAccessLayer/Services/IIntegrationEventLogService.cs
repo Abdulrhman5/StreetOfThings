669815b4b7731656b7f42d91741e44feebc4c80b
Abdulrhman <Abdoulrhmanit@gmail.com>
2020-09-14 15:05:18 +0300

    Add EventLog.DataAccessLayer

---
+++ BuildingBlocks/EventLog.DataAccessLayer/Services/IIntegrationEventLogService.cs
@@ -0,0 +1,19 @@
+﻿using EventBus;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventLog.DataAccessLayer
+{
+    public interface IIntegrationEventLogService
+    {
+        Task<IEnumerable<IntegrationEventLogEntry>> RetrieveEventLogsPendingToPublishAsync(Guid transactionId);
+        Task SaveEventAsync(IntegrationEvent @event, IDbContextTransaction transaction);
+        Task MarkEventAsPublishedAsync(Guid eventId);
+        Task MarkEventAsInProgressAsync(Guid eventId);
+        Task MarkEventAsFailedAsync(Guid eventId);
+    }
+}
