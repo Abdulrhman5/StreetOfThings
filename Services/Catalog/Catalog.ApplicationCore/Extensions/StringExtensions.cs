80356f0f579adecdb59336f361fce3a2c1ba304d
Abdulrhman <Abdoulrhmanit@gmail.com>
2020-11-07 12:29:00 +0200

    Add Extensions, Interfaces, Servces to Catalog.ApplicationCore

---
+++ Services/Catalog/Catalog.ApplicationCore/Extensions/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.ApplicationCore.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        /// <summary>
+        /// Equals Ignoring the case
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="value2"></param>
+        /// <returns></returns>
+        public static bool EqualsIC(this string value, string value2)
+        {
+            return value.Equals(value2, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using Catalog.ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.ApplicationLogic.Interfaces
+{
+    public interface IRepository<TKey, TEntity> where TEntity : class, IEntity<TKey>
+    {
+        public IQueryable<TEntity> Table { get; }
+
+        public TEntity Get(TKey id);
+
+        public TEntity Update(TEntity entity);
+
+        public TEntity Add(TEntity entity);
+
+        public void AddRange(List<TEntity> entities);
+
+        public void Delete(TEntity entity);
+
+        public void Delete(TKey id);
+
+        public void SaveChanges();
+
+        public Task SaveChangesAsync();
+
+    }
+}
