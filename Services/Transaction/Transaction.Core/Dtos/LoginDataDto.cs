f3c6464f2693952b6e4f9019367b3b4ee3f6c034
Abdulrhman <Abdoulrhmanit@gmail.com>
2020-11-14 22:27:00 +0200

    Transaction.Core Add IRepository, IUserDataManager

---
+++ Services/Transaction/Transaction.Core/Dtos/LoginDataDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Transaction.Core.Dtos
+{
+    public class LoginDataDto
+    {
+        public string TokenId { get; set; }
+
+        public string UserId { get; set; }
+
+        public string AccessToken { get; set; }
+    }
+}
---
+++ Services/Transaction/Transaction.Core/Interfaces/IUserDataManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Transaction.Core.Dtos;
+using Transaction.Domain.Entities;
+
+namespace Transaction.Core.Interfaces
+{
+    public interface IUserDataManager
+    {
+        Task<(Login, User)> AddCurrentUserIfNeeded();
+        Task<(Login, User)> AddUserIfNotExisted(Guid tokenId, string originUserId, string accessToken);
+
+        Task<User> AddUserIfNeeded(string userId);
+
+        LoginDataDto GetCuurentUser();
+
+    }
+}
