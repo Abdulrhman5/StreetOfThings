86de255e29dcc378bafe5f213c9b825c7b0a831b
Abdulrhman <Abdoulrhmanit@gmail.com>
2020-11-25 01:14:46 +0200

    Transaction.Core Add TransactionTodayQuery, TransactionstatsOverMonthQuery, TransactionStatsOverYearQuery

---
+++ Services/Transaction/Transaction.Core/Dtos/TransactionMonthlyStatsDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Transaction.Core.Dtos
+{
+    class TransactionMonthlyStatsDto
+    {
+        public int Count { get; set; }
+
+        public DateTime Day { get; set; }
+    }
+}
---
+++ Services/Transaction/Transaction.Core/Dtos/TransactionTodayStatsDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Transaction.Core.Dtos
+{
+    public class TransactionTodayStatsDto
+    {
+        public List<int> TransactionsOverToday { get; set; }
+
+        public int LateReturn { get; set; }
+
+        public int OnTimeReturn { get; set; }
+
+        public int NotReturnedYet { get; set; }
+    }
+}
