7468e099b3e519c79158c419e7b3ed063ac8b0dc
Abdulrhman <Abdoulrhmanit@gmail.com>
2020-11-28 22:08:21 +0200

    MobileGateway Add Api usage guide

---
+++ Gateways/MobileApiGateway/Pages/Documentation/V1/AddComment.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MobileApiGateway
+{
+    public class AddCommentModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Gateways/MobileApiGateway/Pages/Documentation/V1_1/ObjectListV1_1.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MobileApiGateway.Pages.Documentation.V1_1
+{
+    public class ObjectListV1_1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
